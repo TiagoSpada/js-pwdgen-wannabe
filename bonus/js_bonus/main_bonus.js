0fa34a61b8936656471d8c91edc6affba927f5f2
TiagoSpada <tiago.spada.ts@gmail.com>
2024-04-23 17:40:21 +0200

    [UPD] added second bonus (random number)

--- bonus/js_bonus/main_bonus.js
+++ bonus/js_bonus/main_bonus.js
@@ -1,4 +1,5 @@
 let password;
+let RandomNumber;
 const UserName = prompt('Inserisci il tuo nome');
 console.log(UserName);
 const UserLastName = prompt ('Inserisci il tuo cognome');
@@ -6,6 +7,8 @@ console.log(UserLastName);
 const UserFavoriteColor = prompt('Inserisci il tuo cognome preferito');
 console.log(UserFavoriteColor);
 password = `${UserName}${UserLastName}${UserFavoriteColor}`;
-console.log(`${password}21`);
-document.getElementById("password").innerHTML = `${password}21`;
+RandomNumber = Math.floor(Math.random() * 100);
+console.log(RandomNumber);
+console.log(`${password}${RandomNumber}`);
+document.getElementById("password").innerHTML = `${password}${RandomNumber}`;
 
