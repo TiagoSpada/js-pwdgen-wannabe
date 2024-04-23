f256ada40f6ae10bd38963fdc7b388b63a58bbd5
TiagoSpada <tiago.spada.ts@gmail.com>
2024-04-23 17:31:00 +0200

    [UPD] added js for ask name lastname and color

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
 let password;
 const UserName = prompt('Inserisci il tuo nome');
-console.log(UserName);
 const UserLastName = prompt ('Inserisci il tuo cognome');
-console.log(UserLastName);
 const UserFavoriteColor = prompt('Inserisci il tuo cognome preferito');
-console.log(UserFavoriteColor);
 password = `${UserName}${UserLastName}${UserFavoriteColor}`;
-console.log(`${password}21`);
 document.getElementById("password").innerHTML = `${password}21`;
 
